555a8636c6059ca38c9d3e28acb6c8b669a39ac2
Olivier <Himmreich@users.noreply.github.com>
2023-09-05 15:39:37 +0200

    Add files via upload

---
+++ src/main/java/com/example/jaxb17/Jaxb17Application.java
@@ -0,0 +1,13 @@
+package com.example.jaxb17;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Jaxb17Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Jaxb17Application.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/jaxb17/Jaxb17ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.jaxb17;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Jaxb17ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
